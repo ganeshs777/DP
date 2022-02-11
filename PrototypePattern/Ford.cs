e67cf2b5049e3c727b853cdec400d423ecef551f
ganeshs777 <41064935+ganeshs777@users.noreply.github.com>
2022-02-11 16:05:07 +0530

    4

---
+++ PrototypePattern/Ford.cs
@@ -0,0 +1,15 @@
+﻿namespace PrototypePattern
+{
+    public class Ford : BasicCar
+    {
+        public Ford(string m)
+        {
+            ModelName = m;
+        }
+
+        public override BasicCar Clone()
+        {
+            return (Ford)this.MemberwiseClone();
+        }
+    }
+}
